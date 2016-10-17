31ac5c5de30f474f43e28751c8d3f0fa36d29398
Olivier Balais <obalais@gmail.com>
2016-10-17 23:44:17 +0200

    Retrieve public IP from ipify API

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"bytes"
 
 	"github.com/gorilla/mux"
 )
@@ -31,9 +33,11 @@ func GetNetworkInterface(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	interfaceName := vars["interfaceName"]
 
+	publicIpAddress := GetNetworkInterfacePublicIp(interfaceName)
+
 	networkInterface := NetworkInterface{
 		Name:     interfaceName,
-		PublicIp: "8.8.8.8",
+		PublicIp: publicIpAddress,
 	}
 
 	json.NewEncoder(w).Encode(networkInterface)
@@ -45,4 +49,21 @@ func Ifup(interfaceName string) {
 
 func Ifdown(interfaceName string) {
         exec.Command("sudo", "ifdown", interfaceName).Run()
+}
+
+func GetNetworkInterfacePublicIp(interfaceName string) string {
+	var (
+		cmdOut []byte
+		err    error
+		ipifyResult struct {
+			Ip  string         `json:"ip"`
+		}
+	)
+
+	if cmdOut, err = exec.Command("curl", "--interface", interfaceName, "https://api.ipify.org?format=json").Output(); err != nil {
+		fmt.Fprintln(os.Stderr, "There was an error running curl --interface command: ", err)
+	}
+	json.NewDecoder(bytes.NewBuffer(cmdOut)).Decode(&ipifyResult)
+
+	return ipifyResult.Ip
 }
